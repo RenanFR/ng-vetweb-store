29625e758a9abae9e798518151cb51ab93b56362
Renan Rodrigues <renanfr1047@gmail.com>
2019-04-16 23:05:35 -0300

    First component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ProductsModule } from './products/products.module';
+import { LoginModule } from './home/login/login.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProductsModule,
+    LoginModule,    
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vetweb-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
